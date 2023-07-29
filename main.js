0b24e31911b317bfdb7f343fbc18ac7e0194ed93
Semion1093 <simo93rus@gmail.com>
2023-07-29 20:19:57 +0600

    function for mobile header added

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function toggleNavVisibility(){
+   var nav = document.querySelector('.header-container nav');
+   var navVisibility = nav.style.visibility;
+
+   nav.style.visibility = navVisibility == "visible" ? 'hidden' : "visible"
+}
