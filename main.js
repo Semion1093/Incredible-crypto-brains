4353836ae4a1c859b9d149667a59a0a98b89169d
Semion1093 <simo93rus@gmail.com>
2023-07-29 20:27:28 +0600

    add id in header-container

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 function toggleNavVisibility(){
-   var nav = document.querySelector('.header-container nav');
+   var nav = document.querySelector('#header-container nav');
    var navVisibility = nav.style.visibility;
 
    nav.style.visibility = navVisibility == "visible" ? 'hidden' : "visible";
