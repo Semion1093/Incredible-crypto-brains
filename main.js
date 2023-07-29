23cc1b058b25d48eb56599a5df6fa0fe2bcae11d
Semion1093 <simo93rus@gmail.com>
2023-07-29 20:22:18 +0600

    forgot ';'

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ function toggleNavVisibility(){
    var nav = document.querySelector('.header-container nav');
    var navVisibility = nav.style.visibility;
 
-   nav.style.visibility = navVisibility == "visible" ? 'hidden' : "visible"
+   nav.style.visibility = navVisibility == "visible" ? 'hidden' : "visible";
 }
