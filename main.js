9d23cce2f0cef86f06693427b154328b3200da06
Semion1093 <simo93rus@gmail.com>
2023-08-05 23:40:18 +0600

    slider animation added

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ function setNavVisibility() {
    nav.style.visibility = navVisibility == "visible" ? 'hidden' : "visible";
 }
 
+function doSlide(id) {
+   const marginValue = 675;
+   const newFirstCard = document.querySelector('#reviews .card-item:first-child');
+   let paginationButtons = document.querySelectorAll('#reviews .pagination-item');
+
+   newFirstCard.style.marginLeft = -marginValue * id + 'px'; 
+
+   for (const item of paginationButtons) {
+      item.className = item.id == id ? 'pagination-item white' : 'pagination-item grey';
+   }
+}
+
 function expandElement(id) {
    let button = document.getElementById(id);
    let question = button.parentElement.parentElement.childNodes[3];
@@ -15,13 +27,5 @@ function expandElement(id) {
 
    let display = question.style.display;
    question.style.display = display == "block" ? "none" : "block";
-}
-
-function doSlide(id) {
-   let paginationButtons = document.querySelectorAll('#reviews .pagination-item');
-
-   for (const item of paginationButtons) {
-      item.className = item.id == id ? 'pagination-item white' : 'pagination-item grey';
-   }
 }
 
